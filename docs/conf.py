5116535c0c32b40500a057e11c8b68945ae4b58a
BIGWangYuDong <yudongwang@tju.edu.cn>
2021-08-16 10:41:54 +0800

    make read the doc

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ author = 'Yudong Wang'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
